1527d7c07171645ef0879fc004c941a562aa4f14
Alex Rossiter <terrenmoe@gmail.com>
2019-12-08 14:56:16 -0800

    Added to the codepoints object export

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 #!/usr/bin/node
 'use strict';
-
-process.on('unhandledRejection', (err) => {
-  throw err;
-});
+process.on('unhandledRejection', (err) => { throw err; });
 
 const [/* basename*/, /* scriptname*/, ...pparams] = process.argv;
+const { strungOut, codepoints, codepointMap } = require('./src/strungOut.js');
 
-const {
-  strungOut,
-  codepoints,
-  codepointMap
-} = require('./src/strungOut.js');
-
+// process.stdout.write
 console.log(strungOut(...pparams));
-console.log(strungOut([...codepoints].slice(0x41, 0x5b).join(' ')));
-console.log(strungOut(codepointMap.get(0x2200)));
+process.stdout.write(strungOut([...codepoints].slice(0x41, 0x5b).join(' ')));
+process.stdout.write(strungOut(codepointMap.get(0x2200)));
--- src/strungOut.js
+++ src/strungOut.js
@@ -2,40 +2,39 @@
 
 /**
  * @func strungOut
- * @params strings
- * @returns a string list formed from params
- * @returns {string}
+ * @param {string} strungOutput a gathered collection of strings
+ * @returns {string} list formed from gathered args
  */
 const strungOut = (...strungOutput) =>
   strungOutput.reduce(
-    (acc, cur, idx) => acc.concat(`Index${idx}: ${cur.split('.')}`),
+    (acc, cur, idx) => [...acc, `Index${idx}: ${cur.split('')}`],
     []
   );
 
 /**
- * @func
- * @params strings
+ * @const {Object} codepoints
+ * @params {string}
  * @return a string valued iterator formed from the unicode points in seq
- */
-// needs .next() method which returns an object with two props:
-// @value: the next value in the sequence
-// @done: boolean status of iterator completion, true if last
-// value in sequence has been consumed.
-// @return the value prop when done===true for iterator
+ * needs .next() method which returns an object with two props:
+ * @value: the next value in the sequence
+ * @done: bool of iterator completion, true if last value has been consumed.
+ * @return the value prop when done===true for iterator
+**/
 const codepointMap = new Map(new Array(0xffff).fill(1).map(
   (cur, idx) => [idx, String.fromCodePoint(idx)]
 ));
 
 const codepoints = {
-  codepointMap,
-  start: 0,
-  step: 1,
   *[Symbol.iterator]() {
-    for(let codepoint of codepointMap.values()) {
-      yield codepoint;
-      this.start += this.step;
-    }
-    this.start = 0;
+    for(const codepoint of codepointMap.values()) yield codepoint;
+  },
+  gen: (function *gen() {
+    let yieldIn;
+    for(const codepoint of codepointMap.values()) yieldIn = yieldIn ? yield '' : yield codepoint;
+    return { done: true, value: undefined };
+  }()),
+  skip(n) {
+    for(let i = 0; i < n; i++) this.gen.next();
   }
 };
 
